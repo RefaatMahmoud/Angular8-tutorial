88adcafc67baf617bdf1ac31f46da5b6af4edb43
refaat <refaat.aish@edigits.net>
2019-12-23 02:16:50 +0200

    ts-ignore

--- src/app/directives/input-upper-format.directive.spec.ts
+++ src/app/directives/input-upper-format.directive.spec.ts
@@ -2,6 +2,7 @@ import { InputUpperFormatDirective } from './input-upper-format.directive';
 
 describe('InputUpperFormatDirective', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     const directive = new InputUpperFormatDirective();
     expect(directive).toBeTruthy();
   });
